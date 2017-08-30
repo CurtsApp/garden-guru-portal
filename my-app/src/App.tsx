c1f5d717e1e84f625c1ea9388ef92bdbc8c8921f
Curtis Sapp <curtmsapp@gmail.com>
2017-08-30 13:13:22 -0700

    Learning react vocabulary

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,14 +6,20 @@ const logo = require('./logo.svg');
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      // className specifies a css class
+        <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> and save to reload!! :D.
         </p>
+            <div className="App-header">
+                <p>
+                    This is a paragraph.
+                </p>
+            </div>
       </div>
     );
   }
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -4,6 +4,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// Renders an App element to the itemId root in our index.html
+// index.html is a blank page containing root so that we can dynamically render the page through our App
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
